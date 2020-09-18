5453415e900fc3417eba8810cf2ac411c97a9e29
David Zhang <daohanzhang@gmail.com>
2020-09-17 23:27:43 -0400

    add auxiliary files

---
+++ app/data/__init__.py
@@ -0,0 +1,1 @@
+from app.data.stock_info import StockInfo
---
+++ app/data/stock_info.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from typing import Deque
+from collections import deque
+from app.models import Candlestick, IndicatorInfo, PriceInfo
+
+# abstract class for any type of stock info
+DEFAULT_UPDATE_INTERVAL = 60
+
+class StockInfo(ABC):
+    def __init__(self, symbol: str, range: int):
+        self.symbol = symbol
+        # ring buffer-type data structure via queue
+        self.price_buffer: Deque[Candlestick] = deque(maxlen=range)
+        self.indicator_buffer: Deque[IndicatorInfo] = deque(maxlen=range)
+        # how often the stock info should update itself - can be overridden by inheriting class
+        self.update_interval = DEFAULT_UPDATE_INTERVAL
+
+    # method to retrieve updated price info for the interval
+    @abstractmethod
+    def fetch_prices(self) -> PriceInfo:
+        pass
+
+    @abstractmethod
+    def _add_price(self, candle: Candlestick) -> None:
+        self.price_buffer.append(candle)
+
+    def _add_indicator(self, indicator: IndicatorInfo) -> None:
+        self.indicator_buffer.append(indicator)
